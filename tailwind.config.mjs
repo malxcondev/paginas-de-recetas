7ba45a5989b6a7930b45d1a6f311b3b8406315e4
Malxcon <malxcondev@hotmail.com>
2024-02-24 20:28:41 -0500

    segundo commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors:{
+				'Nutmeg': 'hsl(14, 45%, 36%)',
+				'Dark Raspberry': 'hsl(332, 51%, 32%)',
+				'White': 'hsl(0, 0%, 100%)',
+				'Rose White': 'hsl(330, 100%, 98%)',
+				'Eggshell': 'hsl(30, 54%, 90%)',
+				'Light Grey': 'hsl(30, 18%, 87%)',
+				'Wenge Brown': 'hsl(30, 10%, 34%)',
+				'Dark Charcoal': 'hsl(24, 5%, 18%)'
+			}
+		},
+	},
+	plugins: [],
+}
